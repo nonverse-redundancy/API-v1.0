c005fcaad4b12e9cb3d04e5b3ddb54dea3d97aad
Isuru Abhayaratne <isuru2003a@gmail.com>
2021-08-12 00:04:24 +1000

    Initialize NewKeyController

---
+++ app/Http/Controllers/Api/Service/AuthKey/NewKeyController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers\api\service\authkey;
+
+use App\Http\Controllers\Controller;
+use App\Models\AuthKey;
+use Illuminate\Http\Request;
+
+class NewKeyController extends Controller
+{
+    //
+}
