42b3407b111c52c5373fe0a48352ca0c015e1b8c
Isuru Abhayaratne <isuru2003a@gmail.com>
2021-08-13 02:16:20 +1000

    Add AuthKey generate function

--- app/Http/Controllers/Api/Service/AuthKey/NewKeyController.php
+++ app/Http/Controllers/Api/Service/AuthKey/NewKeyController.php
@@ -3,15 +3,17 @@
 namespace App\Http\Controllers\api\service\authkey;
 
 use App\Http\Controllers\Controller;
-use App\Models\AuthKey;
 use Illuminate\Http\Request;
+use App\Models\AuthKey;
+use App\Models\Service;
+use Illuminate\Support\Facades\Hash;
 
 class NewKeyController extends Controller
 {
     function username(Request $request) {
 
         $request->validate([
-            'username' => 'required',
+            'username' => 'required|unique:services.authme',
         ]);
 
         $mojanguuidurl = 'https://api.mojang.com/users/profiles/minecraft/' . $request->username;
@@ -28,5 +30,32 @@ class NewKeyController extends Controller
         );
 
         return response($response, 200);
+    }
+    
+    function generate(Request $request) {
+        
+        $request->validate([
+            'username' => 'required',
+            'password' => 'min:8|confirmed',
+        ]);
+
+        $user = $request->user();
+        $service = new Service();
+        $key = new AuthKey();
+        
+        $service->uuid = $user->uuid;
+        $service->service_id = "authme.service";
+        $service->service_active = 1;
+        $service->service_user = $request->username;
+        $query1 = $service->save();
+
+        //$key->uuid = $user->uuid;
+        $key->username = strtolower($request->username);
+        $key->realname = $request->username;
+        $key->password = Hash::make($request->password);
+        $key->regdate = round(microtime(true) * 1000);
+        $key->regip = $_SERVER['REMOTE_ADDR'];
+        $key->email = $user->email;
+        $query2 = $key->save();
     }
 }
