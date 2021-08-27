30713d5f948a659c95fd806f58ed47d1895b156e
Isuru Abhayaratne <isuru2003a@gmail.com>
2021-08-27 21:14:25 +1000

    Create AuthKey controller

---
+++ app/Http/Controllers/Api/Service/AuthKey/AuthKeyController.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Http\Controllers\api\service\AuthKey;
+
+use App\Http\Controllers\Controller;
+use App\Models\AuthKey;
+use App\Models\Service;
+use Illuminate\Http\Request;
+
+class AuthKeyController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Request $request)
+    {
+        $user = $request->user();
+        $service = Service::where('uuid', $user->uuid)->get()->whereIn('service_id', 'authme.service')->first();
+        $keystats = AuthKey::where('realname', $service->service_user)->first();
+
+        return json_encode($keystats, JSON_PRETTY_PRINT);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
